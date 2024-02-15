48653fb5cbc96910967f9c593405dab64b4040aa
Jedi-Coder1 <98861141+Jedi-Coder1@users.noreply.github.com>
2024-02-15 06:59:29 -0700

    very bad but works

--- antiKick.py
+++ antiKick.py
@@ -6,12 +6,11 @@ except ImportError:
     print("missing imports! please run 'pip install PyDirectInput' in a terminal")
 
 while True:
-    sleep(1150)
-    print("Send Input To Roblox")
+    sleep(1140)
+    # Get Window Handle
     hwnd = win32gui.FindWindowEx(None, None, None, "Roblox")
     win32gui.ShowWindow(hwnd, win32con.SW_MINIMIZE)
     win32gui.ShowWindow(hwnd, win32con.SW_MAXIMIZE)
     win32gui.SetForegroundWindow(hwnd)
-    dirI.press("esc")
-    dirI.press("esc")
+    dirI.click()
     win32gui.ShowWindow(hwnd, win32con.SW_MINIMIZE)
