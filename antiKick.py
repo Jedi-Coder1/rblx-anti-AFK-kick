c5e85d49cddf3bf847c08d4bde015118697fc94b
Jedi-Coder1 <98861141+Jedi-Coder1@users.noreply.github.com>
2024-02-14 18:01:43 -0700

    this be working

--- antiKick.py
+++ antiKick.py
@@ -1,3 +1,14 @@
-class antikick():
-    def __init__(self) -> None:
-        pass
+from time import sleep
+import win32gui, win32con
+import pydirectinput as dirI
+
+while True:
+    sleep(6)
+    print("Send Input To Roblox")
+    hwnd = win32gui.FindWindowEx(None, None, None, "Roblox")
+    win32gui.ShowWindow(hwnd, win32con.SW_MINIMIZE)
+    win32gui.ShowWindow(hwnd, win32con.SW_MAXIMIZE)
+    win32gui.SetForegroundWindow(hwnd)
+    dirI.press("esc")
+    dirI.press("esc")
+    win32gui.ShowWindow(hwnd, win32con.SW_MINIMIZE)
